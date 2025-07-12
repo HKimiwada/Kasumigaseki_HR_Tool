4ae42074d1214c66e7df18205c86086d94bcebe0
Hiroki Kimiwada <h.kimiwada@kasumigaseki.co.jp>
2025-07-12 09:50:46 +0900

    added text extraction logic

--- data_extraction_utils.py
+++ data_extraction_utils.py
@@ -1,1 +1,8 @@
 # Utility functions for data extraction from Resumes
+import fitz # PyMuPDF
+
+doc = fitz.open("Data/谷口博城_職務経歴書.pdf")
+text = ""
+for page in doc:
+    text += page.get_text()
+print(text)

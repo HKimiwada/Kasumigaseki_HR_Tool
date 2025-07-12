342d344718fcc75b2206950b7b662b46b267223e
Hiroki Kimiwada <hiroki.kimiwada@masason.org>
2025-07-12 18:01:50 +0900

    added code function execution logic

--- data_extraction_utils.py
+++ data_extraction_utils.py
@@ -14,3 +14,10 @@ def extract_text_from_pdf(pdf_path):
     for page in doc:
         text += page.get_text()
     return text
+
+if __name__ == "__main__":
+    # Example usage
+    variable_name = "○設計_小川佳英_ヒューマン【職務経歴書】"  
+    pdf_path = f"Data/{variable_name}.pdf"
+    extracted_text = extract_text_from_pdf(pdf_path)
+    print(extracted_text)
